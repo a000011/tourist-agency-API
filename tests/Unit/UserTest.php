1fa08f5d3ca3afc42a6ac2d18630288f2c453c3a
a000011 <anton.nadikin@gmail.com>
2022-01-03 17:38:50 +0400

    test(User): add useless(for now)

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
